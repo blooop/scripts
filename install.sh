48cae5a4258c40fbc33f48c74e42138693b58e7b
Austin Gregg-Smith <blooop@gmail.com>
2025-02-16 12:46:17 +0000

    update install script

--- install.sh
+++ install.sh
@@ -2,27 +2,36 @@
 
 sudo apt update
 
+#basic dev tools
+sudo apt install -y curl python3-pip tmux byobu neovim git git-lfs fd-find ripgrep pinta nvtop htop net-tools 
+
+# Install package managers
+# Install pixi
+curl -fsSL https://pixi.sh/install.sh | bash
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Install Rust and Cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 #lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
-#basic dev tools
-sudo apt install -y python3-pip tmux byobu neovim git git-lfs filelight ripgrep pinta nvtop htop 
-
-#set up docker development environment tool
-pip install rockervsc 
+#set up rocker development environment tool
+uv tool install rocker 
+uv tool install rockerc 
+uv tool install rockervsc 
 
 snap install slack spotify
 sudo snap install code --classic
 
-#for ifconfig
-sudo apt install net-tools
-
-#better search than find
-sudo apt install fd-find
-ln -s $(which fdfind) ~/.local/bin/fd
+#disk space usage
+sudo apt install filelight
 
 #command line fuzzy search
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -35,14 +44,3 @@ sudo apt install -y dolphin konsole
 sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-stable -y
 sudo apt update
 sudo apt install fsearch
-
-
-#set up pipewire for better bluetooth sound quality
-#pulseaudio from https://gist.github.com/the-spyke/2de98b22ff4f978ebf0650c90e82027e
-
-sudo apt install -y pipewire-media-session- wireplumber pipewire-audio-client-libraries
-systemctl --user --now enable wireplumber.service 
-sudo cp /usr/share/doc/pipewire/examples/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
-sudo apt install -y libldacbt-{abr,enc}2 libspa-0.2-bluetooth pulseaudio-module-bluetooth-
-#reboot now, afterwards check with cmd:
-LANG=C pactl info | grep '^Server Name'
