98598f519731265d5ded0625f33fdede3543d922
Austin Gregg-Smith <blooop@gmail.com>
2025-02-26 14:50:56 +0000

    use pixi for common tools instead of apt, and extract file managers into a separate file

--- install.sh
+++ install.sh
@@ -3,15 +3,22 @@
 sudo apt update
 
 #basic dev tools
-sudo apt install -y curl python3-pip tmux byobu neovim git git-lfs fd-find ripgrep pinta nvtop htop net-tools 
+sudo apt install -y curl python3-pip tmux byobu neovim git git-lfs ripgrep pinta nvtop htop net-tools 
 
-# Install package managers
 # Install pixi
 curl -fsSL https://pixi.sh/install.sh | bash
+
+#install basic dev tools
+pixi global install fzf fd-find ripgrep
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
+
+#set up rocker development environment tool
+uv tool install rocker 
+uv tool install rockerc 
+uv tool install rockervsc 
 
 # Install Rust and Cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -23,25 +30,5 @@ sudo dpkg -i google-chrome-stable_current_amd64.deb
 #lazydocker
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
 
-#set up rocker development environment tool
-uv tool install rocker 
-uv tool install rockerc 
-uv tool install rockervsc 
-
 snap install slack spotify
 sudo snap install code --classic
-
-#disk space usage
-sudo apt install filelight
-
-#command line fuzzy search
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-
-#better file manager with tabs and inbuilt console
-sudo apt install -y dolphin konsole 
-
-#global search as you type gui
-sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-stable -y
-sudo apt update
-sudo apt install fsearch
---
+++ install_filemanagers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#global search as you type gui
+sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-stable -y
+sudo apt update
+sudo apt install fsearch
+
+#disk space usage
+sudo apt install filelight
+
+#better file manager with tabs and inbuilt console
+sudo apt install -y dolphin konsole 
+
