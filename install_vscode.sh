cc9979d84075a4d7edfaaf392bbedc071762f2d5
Austin Gregg-Smith <blooop@gmail.com>
2025-03-05 11:38:49 +0000

    add install_vscode.sh

---
+++ install_vscode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to download and install the latest Visual Studio Code
+
+echo "Downloading the latest VS Code .deb package..."
+wget -O vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+
+echo "Installing Visual Studio Code..."
+sudo dpkg -i vscode.deb
+
+echo "Cleaning up downloaded files..."
+rm vscode.deb
+
+echo "Visual Studio Code installation completed!"
