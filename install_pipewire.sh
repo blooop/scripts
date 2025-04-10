9f04b78e230f8c8e3f051d8af537a75c9850904d
Austin Gregg-Smith <blooop@gmail.com>
2025-04-10 12:09:28 +0100

    fix pipewire script

--- install_pipewire.sh
+++ install_pipewire.sh
@@ -2,12 +2,14 @@
 
 # Install necessary packages
 sudo apt update
-sudo apt install -y curl wget git
 
-# Call the new pipewire installation script
-./install_pipewire.sh
+#set up pipewire for better bluetooth sound quality
+#pulseaudio from https://gist.github.com/the-spyke/2de98b22ff4f978ebf0650c90e82027e
 
-# Install other dependencies
-sudo apt install -y build-essential libssl-dev
-
-echo "Installation complete. Please reboot your system."
+sudo apt install -y pipewire-media-session- wireplumber pipewire-audio-client-libraries
+systemctl --user --now enable wireplumber.service 
+sudo cp /usr/share/doc/pipewire/examples/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
+sudo apt install -y libldacbt-{abr,enc}2 libspa-0.2-bluetooth pulseaudio-module-bluetooth-
+#reboot now, afterwards check with cmd:
+echo "REBOOT YOUR MACHINE NOW and then run:"
+echo "LANG=C pactl info | grep '^Server Name'"
