a53ab1a7edc961c8978c42e725ed282634995ee3
Austin Gregg-Smith <blooop@gmail.com>
2025-06-29 14:07:33 +0100

    update the cat of the public key

--- setup_git_ssh.sh
+++ setup_git_ssh.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ssh-keygen
-cat ~/.ssh/id_rsa.pub
 
 sudo apt install -y git
 
 git config --global user.name "Austin Gregg-Smith"
 git config --global user.email "blooop@gmail.com"
+
+cat ~/.ssh/id_ed25519.pub
