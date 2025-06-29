b6c2103360043861361c2b7e0c670b1c40fc50f7
Austin Gregg-Smith <blooop@gmail.com>
2025-06-29 14:22:05 +0100

    add keyring setup

--- setup_git_ssh.sh
+++ setup_git_ssh.sh
@@ -2,7 +2,7 @@
 
 ssh-keygen
 
-sudo apt install -y git
+sudo apt install -y git gnome-keyring libsecret-1-0 libsecret-1-dev
 
 git config --global user.name "Austin Gregg-Smith"
 git config --global user.email "blooop@gmail.com"
