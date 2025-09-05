cb862404b6d8f234049569a2857a7e843e53c2f0
Austin Gregg-Smith <blooop@gmail.com>
2025-09-05 10:33:08 +0100

    add install_palanteer.sh

---
+++ install_palanteer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Palanteer dependencies..."
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    build-essential libgl1-mesa-dev libxrender-dev git cmake ca-certificates python3-dev python3-setuptools python3-wheel 
+
+echo "Cloning and building Palanteer..."
+if [ ! -d palanteer ]; then
+    git clone https://github.com/dfeneyrou/palanteer.git
+fi
+cd palanteer
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j$(nproc) install
+sudo cp bin/palanteer /usr/local/bin/
+cd ../..
+# rm -rf palanteer
+
+echo "Palanteer installation complete!"
