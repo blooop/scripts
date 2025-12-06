a63d3b8804cb924df774be1cba079a89ed33bb93
Austin Gregg-Smith <blooop@gmail.com>
2025-12-06 12:40:43 +0000

    update instruction

--- install_zerotier.sh
+++ install_zerotier.sh
@@ -6,6 +6,5 @@ set -e
 
 curl -s https://install.zerotier.com | sudo bash
 
-echo "\nZeroTier installation complete."
 echo "To connect to a ZeroTier network, run:"
 echo "sudo zerotier-cli join NETWORK_ID"
