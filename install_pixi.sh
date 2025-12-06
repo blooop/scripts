5993b5367497bc11f33975970ba08fa9943eaec0
Austin Gregg-Smith <blooop@gmail.com>
2025-12-06 10:58:06 +0000

    add chezmoi

--- install_pixi.sh
+++ install_pixi.sh
@@ -12,7 +12,7 @@ source ~/.bashrc
  # Ensure pixi is in PATH for this script
 export PATH="$HOME/.pixi/bin:$PATH"
 
-pixi global install fzf fd-find ripgrep byobu nvtop htop
+pixi global install fzf fd-find ripgrep byobu nvtop htop chezmoi
 
 # Set up fzf key bindings and fuzzy completion
 echo 'eval "$(fzf --bash)"' >> ~/.bashrc
